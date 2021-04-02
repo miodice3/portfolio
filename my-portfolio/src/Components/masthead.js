47b128a744b0984f21b8e0433266b2fd3bf7f5cf
Michael Iodice <miodice3@gmail.com>
2021-04-02 16:02:19 -0600

    edit masthead, remove unused link in header, add links to index.html file to link to icons that are missing

--- my-portfolio/src/Components/masthead.js
+++ my-portfolio/src/Components/masthead.js
@@ -1,11 +1,14 @@
+import mike from '../images/mike.jpg'
+
 function Masthead() {
     return (
         <header className="masthead bg-primary text-white text-center">
             <div className="container d-flex align-items-center flex-column">
 
-                <img className="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." />
+                {/* <img className="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." /> */}
+                <img className="masthead-avatar mb-5" src={mike} alt="..." />
 
-                <h1 className="masthead-heading text-uppercase mb-0">Start Bootstrap</h1>
+                <h1 className="masthead-heading text-uppercase mb-0">Michael Iodice</h1>
 
                 <div className="divider-custom divider-light">
                     <div className="divider-custom-line"></div>
@@ -13,7 +16,7 @@ function Masthead() {
                     <div className="divider-custom-line"></div>
                 </div>
 
-                <p className="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+                <p className="masthead-subheading font-weight-light mb-0">Full Stack Web Developer</p>
             </div>
         </header>
     );
