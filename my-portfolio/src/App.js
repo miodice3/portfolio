1c62a19d551fa71e862e28bcdf7a62a1059354d9
Michael Iodice <miodice3@gmail.com>
2021-04-02 14:18:53 -0600

    add bootstrap elements for portfolio and about section

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,16 @@
 import Header from './Components/header.js'
 import Masthead from './Components/masthead.js'
+import Portfolio from './Components/portfolio.js'
+import About from './Components/about.js'
+
 import './App.css';
 function App() {
   return (
     <div>
       <Header />
       <Masthead />
-
+      <Portfolio />
+      <About />
     </div>
   );
 }
