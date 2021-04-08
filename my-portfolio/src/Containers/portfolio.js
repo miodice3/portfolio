65a810af0890a1612f5e7f5d5149dc79917134d1
Michael Iodice <miodice3@gmail.com>
2021-04-08 07:40:04 -0600

    break out project into component, plug in 2, not yet completely finished

--- my-portfolio/src/Containers/portfolio.js
+++ my-portfolio/src/Containers/portfolio.js
@@ -17,62 +17,14 @@ function Portfolio() {
 
                 <div className="row justify-content-center">
 
-                    {/* <div className="col-md-6 col-lg-4 mb-5">
-                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." />
-                        </div>
-                    </div> */}
-
                     <Emissions />
+                    <Emissions />
+                    <Emissions />
+
+                    <Emissions2 />
+                    <Emissions2 />
                     <Emissions2 />
 
-                    {/* <div className="col-md-6 col-lg-4 mb-5">
-                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src="assets/img/portfolio/cake.png" alt="..." />
-                        </div>
-                    </div> */}
-
-                    <div className="col-md-6 col-lg-4 mb-5">
-                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal3">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src="assets/img/portfolio/circus.png" alt="..." />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6 col-lg-4 mb-5 mb-lg-0">
-                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal4">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src="assets/img/portfolio/game.png" alt="..." />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6 col-lg-4 mb-5 mb-md-0">
-                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal5">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src="assets/img/portfolio/safe.png" alt="..." />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6 col-lg-4">
-                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal6">
-                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                            </div>
-                            <img className="img-fluid" src="assets/img/portfolio/submarine.png" alt="..." />
-                        </div>
-                    </div>
                 </div>
             </div>
         </section>
@@ -82,3 +34,12 @@ function Portfolio() {
   }
   
   export default Portfolio;
+
+                      {/* <div className="col-md-6 col-lg-4 mb-5">
+                        <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                            <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img className="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." />
+                        </div>
+                    </div> */}
